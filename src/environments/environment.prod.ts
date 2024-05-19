f451f042a26a3bd0ac54d6869f884bf4851e10ef
Nagy Marton <ngy.marcy@gmail.com>
2024-05-19 23:28:18 +0200

    kotprog

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   firebase: {
-    projectId: 'webfejl-kotprod',
+    projectId: 'webfejl-kotprog',
     appId: '1:294485868650:web:182bf7aff27588fb57ca13',
-    storageBucket: 'webfejl-kotprod.appspot.com',
+    storageBucket: 'webfejl-kotprog.appspot.com',
     apiKey: 'AIzaSyDbqINBY0n7GC4N64MbA1oAesQWw5M6ZpU',
-    authDomain: 'webfejl-kotprod.firebaseapp.com',
+    authDomain: 'webfejl-kotprog.firebaseapp.com',
     messagingSenderId: '294485868650',
   },
   production: true
